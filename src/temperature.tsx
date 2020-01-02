a59dfac11189640ba57381e77869bb2ba4f45f6c
carson <782610180@qq.com>
2020-01-02 22:25:16 +0800

    test

--- src/temperature.tsx
+++ src/temperature.tsx
@@ -13,6 +13,7 @@ interface ITemperatureState {
 export class Temperature extends React.Component<ITemperatureProps, ITemperatureState> {
     render() {
         return <div>
+
             标题: {this.props.title}
             <input type="text" value={this.props.value} onChange={(e: any) => this.props.tellMeWhereYouFrom(e)}/>
         </div>
