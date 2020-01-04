ad5075f5443f545048b678040d20dd0de70afa2a
carson <782610180@qq.com>
2020-01-04 19:43:47 +0800

    提升状态

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css';
 import {Temperature} from "./temperature";
+import React from "react";
 
 
 interface IAppProps {
@@ -8,26 +9,29 @@ interface IAppProps {
 
 interface IAppState {
     commonText: string;
+    value: string;
 }
 
 export class App extends React.Component <IAppProps, IAppState> {
     constructor(props: IAppProps) {
         super(props);
-        this.setState({
+        this.state = {
+            value: "",
             commonText: ""
-        })
+        };
     }
 
     tellMeYouFrom = (e: any) => {
         this.setState({
-            commonText: e.target.value
-        })
+            value: e.target.value
+        });
     };
 
     render() {
         return <div className="App">
             <h1>React temperature !</h1>
-            <Temperature title="first" />
+            <Temperature title="first" tellMeWhereYouFrom={this.tellMeYouFrom} value={this.state.value}/>
+            {/*<Temperature title="second" />*/}
         </div>
     }
 }
--- src/temperature.tsx
+++ src/temperature.tsx
@@ -2,12 +2,13 @@ import React from "react";
 
 interface ITemperatureProps {
     title: string;
-    // tellMeWhereYouFrom: (e: any) => void;
+    value: string;
+    tellMeWhereYouFrom: (e: any) => void;
 }
 
 interface ITemperatureState {
     msg: string;
-    value: string;
+    // value: string;
 }
 
 export class Temperature extends React.Component<ITemperatureProps, ITemperatureState> {
@@ -16,17 +17,16 @@ export class Temperature extends React.Component<ITemperatureProps, ITemperature
         super(props);
         this.state = {
             msg: 'the water will not boiling',
-            value: '0'
         }
     }
 
     render() {
-        const {value, msg} = this.state;
-        return <div>
+        const {msg} = this.state;
+        return <div style={{width: '500px', height: '200px', margin: '0 auto'}}>
             <span>第{this.props.title}个</span>
-            <input type="text" value={value} onChange={e=>this.setState({value:e.target.value})}/>
+            <input type="text" value={this.props.value} onChange={e => this.props.tellMeWhereYouFrom(e)}/>
 
-            <span>{value < '100' ? msg : 'the water will boiling'}</span>
+            <span>{this.props.value < '100' ? msg : 'the water will boiling'}</span>
         </div>
     }
 }
